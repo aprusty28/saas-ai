d67b9e3c5df9bbcf132e27c9df5368060b9979d3
aprusty28 <aprusty@buffalo.edu>
2024-09-09 16:09:38 -0700

    changed middleware.ts code

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,25 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+// import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-  publicRoutes: ["/", "/api/webhook"]
-});
+// export default clerkMiddleware({
+//   // publicRoutes: ["/", "/api/webhook"]
+//   publicRoutes: ['/', 'api/webhook'],
+
+// });
+
+// export const config = {
+//   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
+// };
+
+
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+
+const isProtectedRoute = createRouteMatcher(['/',  '/api/webhook(.*)'])
+
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect()
+})
 
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+
+}
